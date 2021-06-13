a487a33cec5c6c7d850338fc4c3bccec657c1b80
이철용 <ypopp1116@naver.com>
2021-06-14 01:41:27 +0900

    Student Search

---
+++ Student_search/module1_20210415_test/function.cpp
@@ -0,0 +1,74 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+//#include <struct.h>
+#define total_student 27
+
+void SEARCH(student s[total_student])
+{
+	char name[50];
+	char gender[50];
+	int birthday;
+	char num[50];
+	char inc[50];
+	while (1)
+	{
+		int search_num;
+		printf("========================\n");
+		printf("1 : 이름\n");
+		printf("2 : 성별\n");
+		printf("3 : 생일\n");
+		printf("4 : 휴대폰번호\n");
+		printf("5 : 매칭기업\n");
+		printf("========================\n");
+		printf("1~5 선택하세요 : \n");
+		scanf("%d", &search_num);
+		switch (search_num)
+		{
+		case 1:	
+			printf("검색할 이름 : ");
+			scanf("%s", name);
+			for (int i = 0; i <= total_student; i++)
+			{
+				if (s[i].name == name)
+					printf("%s %s %d %s %s", s[i].name, s[i].gender, s[i].birthday, s[i].num, s[i].inc);
+			}
+			break;
+		case 2:
+			printf("검색할 성별 : ");
+			scanf("%s", gender);
+			for (int i = 0; i <= total_student; i++)
+			{
+				if (s[i].gender[i] == gender)
+					printf("%s %s %d %s %s", s[i].name, s[i].gender, s[i].birthday, s[i].num, s[i].inc);
+			}
+			break;
+		case 3:
+			printf("검색할 생일 : ");
+			scanf("%d", &birthday);
+			for (int i = 0; i <= total_student; i++)
+			{
+				if (s[i].birthday[i] == birthday)
+					printf("%s %s %d %s %s", s[i].name, s[i].gender, s[i].birthday, s[i].num, s[i].inc);
+			}
+			break;
+		case 4:
+			printf("검색할 휴대폰 번호 : ");
+			scanf("%d", num);
+			for (int i = 0; i <= total_student; i++)
+			{
+				if (s[i].num[i] == num)
+					printf("%s %s %d %s %s", s[i].name, s[i].gender, s[i].birthday, s[i].num, s[i].inc);
+			}
+			break;
+		case 5:
+			printf("검색할 매칭기업 : ");
+			scanf("%d", inc);
+			for (int i = 0; i <= total_student; i++)
+			{
+				if (s[i].inc[i] == inc)
+					printf("%s %s %d %s %s", s[i].name, s[i].gender, s[i].birthday, s[i].num, s[i].inc);
+			}
+			break;
+		}
+	}
+}
---
+++ Student_search/module1_20210415_test/test1.cpp
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include	<stdio.h>
+#define total_student 27
+#include <struct.h>
+
+
+int main(void)
+{
+	FILE *ifp;
+	FILE *ofp;
+
+	char name[50];
+	char gender[50];
+	int birth;
+	char phone[50];
+	char inc[50];
+	int res;
+
+	ifp = fopen("profile.txt", "r");
+	if (ifp == NULL)
+	{
+		printf("입력 파일을 열지 못헸습니다.\n");
+		return 1;
+	}
+
+	ofp = fopen("succes.txt", "w");
+	if (ofp == NULL)
+	{
+		printf("출력 파일을 열지 못헸습니다.\n"); 
+		return 1;
+	}
+	/*
+	while (1)
+	{
+		res = fscanf(ifp, "%s%s%d%s%s", name, gender, &birth, phone, inc);
+		if (res == EOF)
+		{
+			break;
+		}
+		fprintf(ofp, "%s%5s%5d%5s%5s\n", name, gender, birth, phone, inc);
+	}
+	*/
+	struct student s[total_student];
+	for (int i = 0; i <= total_student; i++)
+	{
+
+	}
+
+	fclose(ifp);
+	fclose(ofp);
+
+	return 0;
+}
